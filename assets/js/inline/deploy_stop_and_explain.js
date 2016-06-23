3adbd05507dc1454736b9bf265fb7b23fedba12e
Tim <dev@tim.ph>
2016-06-22 20:39:20 -0400

    update formatting for coding standards

--- assets/js/inline/deploy_stop_and_explain.js
+++ assets/js/inline/deploy_stop_and_explain.js
@@ -1,33 +1,33 @@
 // Move 'stop and explain' to the top of the page.
-var stop_and_explain = jQuery('div#stop_and_explain');
-jQuery(stop_and_explain).insertBefore(jQuery('div#deploy_status')).slideToggle(
-		1000);
+var stop_and_explain = jQuery( 'div#stop_and_explain' );
+jQuery( stop_and_explain ).insertBefore( jQuery( 'div#deploy_status' ) )
+	.slideToggle( 1000 );
 
 // Remove the loading graphic.
-jQuery('#deploy_status .boldgrid-loading').slideUp();
+jQuery( '#deploy_status .boldgrid-loading' ).slideUp();
 
 // Update the title of the page, which also removes the spinner.
-jQuery('#deploy_status h1').html('Installation complete!').prepend(
-		"<span class='dashicons dashicons-yes'></span>");
+jQuery( '#deploy_status h1' ).html( 'Installation complete!' )
+	.prepend( '<span class="dashicons dashicons-yes"></span>' );
 
 // Remove the installation text, the one that updates on the fly.
-jQuery('#deploy_text').addClass('hidden');
-jQuery('.spinner').remove();
+jQuery( '#deploy_text' ).addClass( 'hidden' );
+jQuery( '.spinner' ).remove();
 
 // Scroll the user to the top of the page.
-jQuery('html, body').animate({
+jQuery( 'html, body' ).animate({
 	scrollTop : 0
-}, 'slow');
+}, 'slow' );
 
 //Reach out and hit the front end of the site to make sure all after theme switch hooks are fired
 jQuery.ajax({
 	url: IMHWPB.configs.site_url,
 });
 
-jQuery(function() {
+jQuery( function() {
 	// After the page is done loading, update the cart total.
-	if (typeof boldgrid_deploy_cost != 'undefined') {
+	if ( typeof boldgrid_deploy_cost != 'undefined' ) {
 		var BaseAdmin = new IMHWPB.BaseAdmin();
-		BaseAdmin.update_header_cart(boldgrid_deploy_cost);
+		BaseAdmin.update_header_cart( boldgrid_deploy_cost );
 	}
 });
