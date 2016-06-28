4e9bcca958dd48808a50fc7e1ca5389ae4a00fd1
Tim <dev@tim.ph>
2016-06-28 18:44:41 -0400

    fixing single double quote mixing.

--- pages/templates/boldgrid-inspirations-design-first.php
+++ pages/templates/boldgrid-inspirations-design-first.php
@@ -1,5 +1,5 @@
 <script type="text/html" id="tmpl-init-categories">
-	<div class='category-filter' ><?php echo __( 'Category filter', 'boldgrid-inspirations' ); ?></div>
+	<div class="category-filter" ><?php echo __( 'Category filter', 'boldgrid-inspirations' ); ?></div>
 
 	<div class="sub-category active">
 		<input type="radio" name="sub-category" checked data-sub-category-id="0" >
@@ -19,21 +19,20 @@
 	<# }); #>
 </script>
 
-<script type='text/html' id='tmpl-theme'>
-	<#
-		// Format our theme title.
+<script type="text/html" id="tmpl-theme">
+	<# // Format our theme title.
 		data.build.ThemeName = data.build.ThemeName.replace( 'boldgrid-', '' );
 		data.key = IMHWPB.configs.api_key
 	#>
 	<div class="theme" tabindex="0" data-category-id="{{data.build.ParentCategoryId}}" data-sub-category-id="{{data.build.CategoryId}}" data-sub-category-title="{{data.build.SubCategoryName}}" data-page-set-id="{{data.build.PageSetId}}" data-theme-id="{{data.build.ThemeId}}" data-theme-title="{{data.build.ThemeName}}">
 
 		<div class="theme-screenshot">
-			<img class='lazy' data-original="{{data.configs.asset_server}}/api/asset/get?key={{data.configs.api_key}}&id={{data.build.AssetId}}" alt="" width='290' height='194'>
+			<img class="lazy" data-original="{{data.configs.asset_server}}/api/asset/get?key={{data.configs.api_key}}&id={{data.build.AssetId}}" alt="" width="290" height="194">
 		</div>
 
 		<h2 class="theme-name" >
-			<span class='name'>{{data.build.ThemeName}}</span>
-			<span class='sub-category-name'>- {{data.build.SubCategoryName}}</span>
+			<span class="name">{{data.build.ThemeName}}</span>
+			<span class="sub-category-name">- {{data.build.SubCategoryName}}</span>
 		</h2>
 
 		<div class="theme-actions">
@@ -42,18 +41,15 @@
 	</div>
 </script>
 
-<script type='text/html' id='tmpl-pagesets'>
-	<#
-		_.each( data, function( pageset ) {
-			pageset.is_default = pageset.is_default_page_set;
-			pageset.is_default_page_set = ( '1' === pageset.is_default_page_set ? 'checked' : '' );
-			pageset.page_set_description = pageset.page_set_description.replace(/'/g, "&#39;");
+<script type="text/html" id="tmpl-pagesets">
+	<# _.each( data, function( pageset ) {
+		pageset.is_default = pageset.is_default_page_set;
+		pageset.is_default_page_set = ( '1' === pageset.is_default_page_set ? 'checked' : '' );
+		pageset.page_set_description = pageset.page_set_description.replace(/'/g, "&#39;");
 	#>
-		<div class='pageset-option' title='{{pageset.page_set_description}}'>
+		<div class="pageset-option" title="{{pageset.page_set_description}}">
 			<input type="radio" name="pageset" data-is-default="{{pageset.is_default}}" data-page-set-id="{{pageset.id}}" {{pageset.is_default_page_set}} >
-			<span class='pointer'>{{pageset.page_set_name}}</span>
+			<span class="pointer">{{pageset.page_set_name}}</span>
 		</div>
-	<#
-		});
-	#>
+	<# }); #>
 </script>
