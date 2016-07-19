10aad5d0f8f20da7d943e86af8ae07809c6a1c6a
Tim <dev@tim.ph>
2016-07-19 10:35:58 -0400

    move api key link under submission box.

--- includes/class-boldgrid-inspirations.php
+++ includes/class-boldgrid-inspirations.php
@@ -555,7 +555,6 @@ class Boldgrid_Inspirations {
 <div id="container_boldgrid_api_key_notice" class="error">
 	<div class="api-notice">
 		<h2 class="dashicons-before dashicons-admin-network"><?php _e( 'Enter Your BoldGrid Connect Key' ); ?></h2>
-		<a href="#" class="boldgridApiKeyLink"><?php _e( 'Don\'t have an API key yet or lost your key?' ); ?></a><br /><br />
 		<p id="boldgrid_api_key_notice_message">
 			Please enter your <b>32 digit BoldGrid Connect Key</b> below and click
 			submit.
@@ -568,6 +567,8 @@ class Boldgrid_Inspirations {
 			<button id="submit_api_key" class="button button-primary">Submit</button>
 			<span><div id="boldgrid-api-loading" class="boldgrid-wp-spin"></div></span>
 		</form>
+		<br />
+		<a href="#" class="boldgridApiKeyLink"><?php _e( 'Don\'t have an API key yet or lost your key?' ); ?></a>
 	</div>
 	<div class="new-api-key hidden">
 		<h2 class="dashicons-before dashicons-admin-network"><?php _e( 'Request a BoldGrid Connect Key' ); ?></h2>
