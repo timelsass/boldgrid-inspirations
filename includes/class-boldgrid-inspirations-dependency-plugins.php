573b6429a64f512b4fac09a3cf75e3f744f5aa2c
Tim <dev@tim.ph>
2016-09-23 14:25:04 -0400

    remove notice if no dependancy plugins

--- includes/class-boldgrid-inspirations-dependency-plugins.php
+++ includes/class-boldgrid-inspirations-dependency-plugins.php
@@ -601,7 +601,7 @@ class Boldgrid_Inspirations_Dependency_Plugins {
 
 		$only_staging = ( count( $this->dependent_plugins_not_installed ) === 1 && implode( $this->dependent_plugins_not_installed ) === 'boldgrid-staging/boldgrid-staging.php' ) ? true : false;
 
-		if ( ! isset( $_POST['boldgrid-plugin-install'] ) && false === $only_staging ) {
+		if ( ! isset( $_POST['boldgrid-plugin-install'] ) && ! empty( $this->dependent_plugins_not_installed ) && ! $only_staging ) {
 			return true;
 		}
 
