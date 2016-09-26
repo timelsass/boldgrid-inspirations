5ebfe93cc81b183a6b7739cf4b655686bd73a042
Tim <dev@tim.ph>
2016-09-26 14:09:24 -0400

    display iframe all the time so gallery can calculate properly.

--- assets/js/boldgrid-inspirations.js
+++ assets/js/boldgrid-inspirations.js
@@ -85,11 +85,6 @@ IMHWPB.InspirationsDesignFirst = function( $, configs ) {
 	 * @since 1.2.3
 	 */
 	this.chooseTheme = function( ) {
-		// Immediately hide the iframe to give a better transition effect.
-		$( '#screen-content iframe#theme-preview' )
-			.addClass( 'hidden' )
-			.css( 'display', '' );
-
 		// Load the theme title and sub category title.
 		$( '#sub-category-title' ).html( '- ' + self.$theme.closest( '.theme' ).attr( 'data-sub-category-title' ) );
 		$( '#theme-title' ).html( self.$theme.closest( '.theme' ).attr( 'data-theme-title' ) );
@@ -951,9 +946,7 @@ IMHWPB.InspirationsDesignFirst = function( $, configs ) {
 
 		// Load our loading graphic.
 		$( '#build-cost' ).animate( { opacity: 0 }, 400 );
-		$( '#screen-content iframe#theme-preview' ).fadeOut( function() {
-			$( '#screen-content .boldgrid-loading' ).fadeIn();
-		} );
+		$( '#screen-content .boldgrid-loading' ).fadeIn();
 
 		successAction = function( msg ) {
 			var $screenContent = $( '#screen-content' ),
