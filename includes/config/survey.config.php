d68e705ca731fd853c598bcbcdea77e2139bb59c
BradM <bradm@inmotionhosting.com>
2017-01-12 10:44:50 -0500

    (2) Update:              JIRA WPB-2763   Update email and address on Contact Us page.

---
+++ includes/config/survey.config.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * An array of config settings for the Inspirations survey.
+ *
+ * @since 1.3.6
+ */
+
+$email = Boldgrid_Inspirations_Survey::get_value( 'email' );
+$address = Boldgrid_Inspirations_Survey::get_value( 'address' );
+$map_iframe = sprintf(
+	'<iframe style="width:100%%;height:100%%;" src="https://maps.google.com/maps?q=%1$s&amp;t=m&amp;z=16&amp;output=embed" frameborder="0"></iframe>',
+	urlencode( $address )
+);
+
+return array(
+	'find_and_replace' => array(
+		array(
+			'removal_key' =>		'phone',
+			'value' =>				Boldgrid_Inspirations_Survey::get_value( 'phone' ),
+			'display' =>			Boldgrid_Inspirations_Survey::should_display( 'phone' ),
+			'on_success' =>			'node_value',
+		),
+		array(
+			'removal_key' =>		'address',
+			'value' =>				Boldgrid_Inspirations_Survey::get_value( 'address' ),
+			'display' =>			Boldgrid_Inspirations_Survey::should_display( 'address' ),
+			'on_success' =>			'node_value',
+		),
+		array(
+			'removal_key' =>		'email',
+			'value' =>				$email,
+			'display' =>			Boldgrid_Inspirations_Survey::should_display( 'email' ),
+			'parent_attributes' =>	array( 'href' => 'mailto:' . $email, ),
+			'on_success' =>			'node_value',
+		),
+		array(
+			'removal_key' =>		'map',
+			'value' =>				$map_iframe,
+			'display' =>			Boldgrid_Inspirations_Survey::get_value( 'address', 'map' ) && Boldgrid_Inspirations_Survey::should_display( 'address' ),
+			'on_success' =>			'append_child',
+		),
+	)
+);
+?>
