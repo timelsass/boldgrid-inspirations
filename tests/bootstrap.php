01f716ff82e1e7de9e7738995a5db5381ee9097e
BradM <bradm@inmotionhosting.com>
2017-07-31 10:11:49 -0400

    Tmp fixing tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,9 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
+	// todo Do we need to manually require this file?
+	require_once dirname( dirname( __FILE__ ) ) . '/vendor/boldgrid/library/src/Library/License.php';
+
 	require dirname( dirname( __FILE__ ) ) . '/boldgrid-inspirations.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
