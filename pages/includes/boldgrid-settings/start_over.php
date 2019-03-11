1e29f0a58e3229c61248f6c80a3cc503e56992e0
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2019-03-11 11:02:41 -0400

    There's no start over code for plugins, update the verbiage.

--- pages/includes/boldgrid-settings/start_over.php
+++ pages/includes/boldgrid-settings/start_over.php
@@ -81,7 +81,7 @@ if ( true == $this->user_has_built_a_boldgrid_site() || true == $show_start_over
 	?>
 
 			<hr />
-			<br /> <strong><?php echo esc_html__( 'BoldGrid Plugins and Themes:', 'boldgrid-inspirations' ); ?></strong><br /> <br />
+			<br /> <strong><?php echo esc_html__( 'BoldGrid Themes:', 'boldgrid-inspirations' ); ?></strong><br /> <br />
 
 			<?php if ( is_plugin_active( 'boldgrid-ninja-forms/ninja-forms.php' ) && ( current_user_can( 'delete_plugins' ) ||  function_exists( 'is_multisite' ) && is_multisite() && is_super_admin() ) ) { ?>
 				<input type="checkbox" id="boldgrid_delete_forms" name="boldgrid_delete_forms" value="1" /> <span><?php echo esc_html__( 'Delete all BoldGrid Forms and Entries.', 'boldgrid-inspirations' ); ?></span><br /> <br />
