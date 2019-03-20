35528f43d4bcee8c85a85ecc4cd16db4d2c96352
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2019-03-20 08:34:18 -0400

    Updating version

--- boldgrid-inspirations.php
+++ boldgrid-inspirations.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BoldGrid Inspirations
  * Plugin URI: https://www.boldgrid.com/boldgrid-inspirations/
- * Version: 1.7.0-rc.1
+ * Version: 1.7.0-rc.2
  * Author: BoldGrid <support@boldgrid.com>
  * Author URI: https://www.boldgrid.com/
  * Description: Find inspiration, customize, and launch! BoldGrid Inspirations includes FREE WordPress themes and is the easiest way to launch a new WordPress site complete with custom content.
