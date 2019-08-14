88a395142aa0e96e81752733d73e9be0211af8c3
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2019-08-14 09:44:16 -0400

    Update dashboard widget to new style

--- includes/class-boldgrid-inspirations-dashboard-widget.php
+++ includes/class-boldgrid-inspirations-dashboard-widget.php
@@ -20,33 +20,32 @@ class Boldgrid_Inspirations_Dashboard_Widget {
 	 * @since 2.1.0
 	 */
 	public function add_admin_hooks() {
-		add_filter( 'Boldgrid\Library\Notifications\DashboardWidget\displayWidget\plugin-boldgrid-inspirations', array( $this, 'filter_item' ) );
+		add_filter( 'Boldgrid\Library\Notifications\DashboardWidget\getFeaturePlugin\boldgrid-inspirations', array( $this, 'filter_feature' ), 10, 2 );
 	}
 
 	/**
 	 * Filter the Inspirations item in the dashboard widget.
 	 *
 	 * @since 2.1.0
+	 *
+	 * @param  \Boldgrid\Library\Library\Ui\Feature    The feature object.
+	 * @param  \Boldgrid\Library\Library\Plugin\Plugin The plugin object.
 	 */
-	public function filter_item( $item ) {
-		$item['icon'] = '<img src="https://repo.boldgrid.com/assets/icon-boldgrid-inspirations-128x128.png" />';
+	public function filter_feature( \Boldgrid\Library\Library\Ui\Feature $feature, \Boldgrid\Library\Library\Plugin\Plugin $plugin ) {
+		$feature->icon = '<img src="https://repo.boldgrid.com/assets/icon-boldgrid-inspirations-128x128.png" />';
 
 		if ( ! Boldgrid_Inspirations_Installed::has_built_site() ) {
-			$item['subItems'][] = '
-				<p>
-					<span class="dashicons dashicons-info"></span> ' .
-					wp_kses(
-						sprintf(
-							// translators: 1 The opening anchor tag to the Inspirations page, 2 its closing tag.
-							__( 'It looks like you haven\'t completed the Inspirations process yet. %1$sClick here to finish up and start designing%2$s.', 'boldgrid-inspirations' ),
-							'<a href="' . esc_url( admin_url( 'admin.php?page=boldgrid-inspirations' ) ) . '">',
-							'</a>'
-						),
-						array( 'a' => array( 'href' => array() ) )
-					) . '
-				</p>';
+			$feature->content .= '<div class="notice notice-info inline"><p>' . wp_kses(
+				sprintf(
+					// translators: 1 The opening anchor tag to the Inspirations page, 2 its closing tag.
+					__( 'It looks like you haven\'t completed the Inspirations process yet. %1$sClick here to begin%2$s.', 'boldgrid-inspirations' ),
+					'<a href="' . esc_url( admin_url( 'admin.php?page=boldgrid-inspirations' ) ) . '">',
+					'</a>'
+				),
+				[ 'a' => [ 'href' => [] ] ]
+			) . '</p></div>';
 		}
 
-		return $item;
+		return $feature;
 	}
 }
